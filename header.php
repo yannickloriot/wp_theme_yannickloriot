eeea7883cb56fc8e92e42303c6a3da883822f161
Yannick Loriot <yannick.loriot@gmail.com>
2016-01-26 21:53:07 +0100

    Add meta description at home

--- header.php
+++ header.php
@@ -14,9 +14,15 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"">
+	<?php
+	if (is_front_page() && is_home()) {
+		$tagline = get_bloginfo('description');
+
+		echo "<meta name=\"description\" content=\"$tagline\"/>";
+	}
+	?>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
 	<?php wp_head(); ?>
 </head>
 
